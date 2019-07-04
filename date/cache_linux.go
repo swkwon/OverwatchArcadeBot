2fdc8846b067d70f57af4d61a1c1b67236c7ca38
swkwon <swkwon04@gmail.com>
2019-07-04 15:08:09 +0900

    change file name

--- date/cache_linux.go
+++ date/cache_linux.go
@@ -8,11 +8,11 @@ import (
 func WriteFile() {
 	t := time.Now()
 	str := t.Format(time.RFC3339)
-	ioutil.WriteFile("/tmp/data.log", []byte(str), 0644)
+	ioutil.WriteFile("/tmp/cache_overwatch_arcade_bot.log", []byte(str), 0644)
 }
 
 func ReadFile() string {
-	if b, e := ioutil.ReadFile("/tmp/data.log"); e == nil {
+	if b, e := ioutil.ReadFile("/tmp/cache_overwatch_arcade_bot.log"); e == nil {
 		return string(b)
 	}
 	return ""
--- date/cache_windows.go
+++ date/cache_windows.go
@@ -8,11 +8,11 @@ import (
 func WriteFile() {
 	t := time.Now()
 	str := t.Format(time.RFC3339)
-	ioutil.WriteFile("data.log", []byte(str), 0644)
+	ioutil.WriteFile("cache_overwatch_arcade_bot.log", []byte(str), 0644)
 }
 
 func ReadFile() string {
-	if b, e := ioutil.ReadFile("data.log"); e == nil {
+	if b, e := ioutil.ReadFile("cache_overwatch_arcade_bot.log"); e == nil {
 		return string(b)
 	}
 	return ""
